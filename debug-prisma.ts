b125efb73f0961e89e8a081e704029c3c515eb21
Nishchay <nishchaygithub@gmail.com>
2026-03-03 08:08:45 +0530

    chore: clean repo - remove 22+ test scripts, debug files, load-tests, temp MDs

--- debug-prisma.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from './lib/prisma';
-
-async function main() {
-    console.log('Prisma Models:', Object.keys(prisma).filter(k => !k.startsWith('$') && !k.startsWith('_')));
-}
-
-main()
-    .catch(console.error)
-    .finally(() => prisma.$disconnect());
